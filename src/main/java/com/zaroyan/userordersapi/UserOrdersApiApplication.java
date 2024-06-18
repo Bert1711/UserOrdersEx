22e7367994ed3337e2d82551983e7486144b265f
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-06-18 11:41:10 +0300

    "initial commit"

---
+++ src/main/java/com/zaroyan/userordersapi/UserOrdersApiApplication.java
@@ -0,0 +1,13 @@
+package com.zaroyan.userordersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOrdersApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserOrdersApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zaroyan/userordersapi/UserOrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaroyan.userordersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserOrdersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
